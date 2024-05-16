9d7c7e3de904dbe8c8e5e603fb65336abd17239f
Georgekkh <115355434+Georgekkh@users.noreply.github.com>
2024-05-16 09:46:46 +0000

    Practical_4-Georgekkh

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hello from './Hello.js';
 import Hobby from './Hobby.js';
 import TitleLi from './TitleLi.js';
@@ -6,11 +7,11 @@ function App(props) {
     const name = 'ENTER YOUR NAME';
     return <div>
         <Hello name={name}></Hello>
-//              <ol>
-//                  <TitleLi title="Name">{name}</TitleLi>
-//                  <TitleLi title="Age">18</TitleLi>
-//                  <TitleLi title="Hobbies"></TitleLi>
-//               </ol>
+              <ol>
+                  <TitleLi title="Name">{name}</TitleLi>
+                  <TitleLi title="Age">18</TitleLi>
+                  <TitleLi title="Hobbies"></TitleLi>
+               </ol>
       {props.hobbies.map(function(hobby) {
           return <Hobby title={hobby.title} emoji={hobby.emoji} key={hobby.title} />
       })}
