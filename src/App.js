a09bda7ad6b9eb352899441dd3fb87cc58318b4b
Georgekkh <115355434+Georgekkh@users.noreply.github.com>
2024-05-16 09:48:07 +0000

    Practical_4-Georgekkh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hello from './Hello.js';
 import Hobby from './Hobby.js';
-import TitleLi from './TitleLi.js';
+// import TitleLi from './TitleLi.js';
 
 function App(props) {
     const name = 'ENTER YOUR NAME';
