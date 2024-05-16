4667f20853ca7e378ef19d2d321801f0b4bda96e
Georgekkh <115355434+Georgekkh@users.noreply.github.com>
2024-05-16 09:37:29 +0000

    Practical_4-Georgekkh

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import Hello from './Hello.js';
 import Hobby from './Hobby.js';
 import TitleLi from './TitleLi.js';
 
+function App(props) {
+    const name = 'ENTER YOUR NAME';
+    return <div>
+        <Hello name={name}></Hello>
+//              <ol>
+//                  <TitleLi title="Name">{name}</TitleLi>
+//                  <TitleLi title="Age">18</TitleLi>
+//                  <TitleLi title="Hobbies"></TitleLi>
+//               </ol>
+      {props.hobbies.map(function(hobby) {
+          return <Hobby title={hobby.title} emoji={hobby.emoji} key={hobby.title} />
+      })}
+     </div>
+  }4-Georgekkh
+
 // export default function App() {
 //     const name = 'ENTER YOUR NAME';
 //     return (
@@ -24,17 +39,3 @@ import TitleLi from './TitleLi.js';
 //     );
 // }
 
-function App(props) {
-    const name = 'ENTER YOUR NAME';
-    return <div>
-        <Hello name={name}></Hello>
-//              <ol>
-//                  <TitleLi title="Name">{name}</TitleLi>
-//                  <TitleLi title="Age">18</TitleLi>
-//                  <TitleLi title="Hobbies"></TitleLi>
-//               </ol>
-      {props.hobbies.map(function(hobby) {
-          return <Hobby title={hobby.title} emoji={hobby.emoji} key={hobby.title} />
-      })}
-     </div>
-  }
